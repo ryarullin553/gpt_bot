451c142145df60972028f2f5153deebaadedee1e
Ruslan <ryarullin553@gmail.com>
2023-11-25 23:30:24 +0300

    update gpt connection

--- gpt.py
+++ gpt.py
@@ -1,4 +1,4 @@
-from g4f import ChatCompletion
+from g4f import ChatCompletion, Provider
 from constants import BotMessage
 
 
@@ -6,8 +6,16 @@ async def get_answer(promt: str) -> str:
     try:
         response = await ChatCompletion.create_async(
             model='gpt-3.5-turbo',
-            messages=[{'role': 'user', 'content': promt}]
+            messages=[{'role': 'user', 'content': promt}],
+            provider=Provider.GptForLove
         )
         return response
     except Exception:
-        return BotMessage.GPT_ERROR
+        try:
+            response = await ChatCompletion.create_async(
+                model='gpt-4',
+                messages=[{'role': 'user', 'content': promt}]
+            )
+            return response
+        except Exception:
+            return BotMessage.GPT_ERROR
