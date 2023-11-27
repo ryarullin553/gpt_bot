2883fc47540473297378774eadea3dce37adb28d
Ruslan <ryarullin553@gmail.com>
2023-11-27 09:16:19 +0300

    change gpt model

--- gpt.py
+++ gpt.py
@@ -5,16 +5,10 @@ from constants import BotMessage
 async def get_answer(promt: str) -> str:
     try:
         response = await ChatCompletion.create_async(
-            model='gpt-4',
+            model='gpt-3.5-turbo',
             messages=[{'role': 'user', 'content': promt}]
         )
     except Exception:
-        try:
-            response = await ChatCompletion.create_async(
-                model='gpt-3.5-turbo',
-                messages=[{'role': 'user', 'content': promt}]
-            )
-        except Exception:
-            response = BotMessage.GPT_ERROR
+        response = BotMessage.GPT_ERROR
 
     return response
